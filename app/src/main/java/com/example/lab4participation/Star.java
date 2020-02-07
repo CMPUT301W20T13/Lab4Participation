78a6c501b06ebf9a7d8846209e15ac032d6e301c
Jun Li <jun@Juns-MacBook-Pro.local>
2020-02-07 12:56:53 -0700

    Updated Star class

--- app/src/main/java/com/example/lab4participation/Star.java
+++ app/src/main/java/com/example/lab4participation/Star.java
@@ -1,6 +1,19 @@
 package com.example.lab4participation;
 
 public class Star extends Shape {
-    
+    private String color;
 
+    public Star(int x, int y, String color) {
+        super(x, y);
+        this.color = color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+    public String getColor() {
+        return color;
+    }
 }
