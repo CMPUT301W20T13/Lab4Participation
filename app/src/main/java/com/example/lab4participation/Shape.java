66753d2c883a09e573d188287dfc2fa3e14ed615
cjpontikes <cjpontikes@gmail.com>
2020-02-07 13:46:30 -0700

    idk anymore

--- app/src/main/java/com/example/lab4participation/Shape.java
+++ app/src/main/java/com/example/lab4participation/Shape.java
@@ -3,8 +3,7 @@ package com.example.lab4participation;
 public abstract class Shape {
     private int x;
     private int y;
-
-        
+	        
     public Shape(int x, int y){
         this.x=x;
         this.y=y;
